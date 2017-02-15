b384d732eff90946cb5eba521b4877908a8df0b5
Aubrey Badger <akbadger@gmail.com>
2017-02-14 20:32:29 -0500

    added comments line by line for understanding

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,30 +1,35 @@
 
 
 // Variable Declarations
-var avow = require('avow');
+var avow = require('avow'); //this adds program avow (true/false program Collin wrote)//
 
 // Functional Declarations
-function FizzBuzz (n) {
-        var output ='';
-        for (var i = 1; i <= n; i++) {
-        if (i % 5 === 0 && i % 3 === 0) {
+function FizzBuzz (n) {   //function is storing statements together to perform a specific task.//
+        var output ='';  //variable set for output, where it will later cancatenate all outputs into a string.//
+        for (var i = 1; i <= n; i++) { //for statement, 
+            //initialization, condition, update
+    //if a number is divisible by 5 with no remainder and divisible by 3 with no remainder, output +(previous output) and fizzbuzz// 
+        if (i % 5 === 0 && i % 3 === 0) { 
             output += 'FizzBuzz';
        }
+    //if a number is divisible by 5 with no remainder, but not divisible by 3 with no remainder, output +(previous output) and buzz//
         else if (i % 5 === 0 && i % 3 !== 0) {
             output += 'Buzz';
        }
+       //if a number is divisible by 3 with no remainder, but not divisible by 5 with no remainder, output +(previous output) and fizz//
         else if (i % 3 === 0 && i % 5 !== 0) {
            output += 'Fizz';
        }
+       //if a number is not divisible by 3 with no remainder, and not divisible by 5 with no remainder, output +(previous output) and .// 
         else /*(n % 3 !== 0 & n % 5 !== 0)*/ {
            output += '.';
        }
     }
-    return output;
+    return output; //this returns data from the function
     
 }
     
-FizzBuzz(15);
+FizzBuzz(15); //calls the function whenever I need it to run
 
 
 
